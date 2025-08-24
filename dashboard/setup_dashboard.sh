10493e26758a7e07b857e82781bb4c16b0026cf1
kettleTeacues <teacues@gmail.com>
2025-08-24 20:50:28 +0900

    install dashbaord with helm

---
+++ dashboard/setup_dashboard.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
+# Add kubernetes-dashboard repository
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+# Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+## https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
+# add admin-user and other resources
+kubectl apply -f accounts.yaml
+
+# generate token
+kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath="{.data.token}" | base64 -d
